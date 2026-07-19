65b2f041f1ee780fe6d816f349c704f4bfe76629
Kirtan Bhatiya <kirtanBhatiya@example.com>
2026-07-19 10:58:21 +0530

    Added Python Code

---
+++ For_loop.py
@@ -0,0 +1,90 @@
+#======================
+# For loop
+#======================
+
+#======================
+#Syntax
+#======================
+
+# for i in range():
+    #logic
+
+#======================
+#print numbers
+#======================
+
+# for i in range(1, 6):
+#     print(i)
+
+#======================
+#print list elements
+#======================
+# fruits = ["Apple", "Banana", "Mango"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+#=====================
+# Print Characters of a String
+#=====================
+# name = "Python"
+
+# for ch in name:
+#     print(ch)
+
+#====================
+#Print Tuple Elements
+#====================
+# numbers = (10, 20, 30, 40)
+
+# for num in numbers:
+#     print(num)
+
+#======================
+# Print Set Elements
+#=====================
+#colors = {"Red", "Green", "Blue"}
+
+# for color in colors:
+#     print(color)
+
+#======================
+#Sum of Numbers
+#======================
+# total = 0
+
+# for i in range(1, 6):
+#     total += i
+
+# print("Sum =", total)
+
+#======================
+#Multiplication Table
+#======================
+# num = 5
+
+# for i in range(1, 11):
+#     print(num, "x", i, "=", num * i)
+
+#======================
+#Nested for Loop
+#======================
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(i, j)
+
+#======================
+#for Loop with break
+#======================
+# for i in range(1, 11):
+#     if i == 6:
+#         break
+#     print(i)
+
+#=====================
+#for Loop with continue
+#=====================
+# for i in range(1, 6):
+#     if i == 3:
+#         continue
+#     print(i)
---
+++ While_loop.py
@@ -0,0 +1,96 @@
+#=========================
+# While Loop
+#=========================
+
+#=========================
+# Syntax
+#=========================
+# while condition:
+    #code to exicute
+
+#=========================
+# Print Numbers from 1 to 5
+#=========================
+
+# i = 1
+
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+#=========================
+# Print Even Numbers
+#=========================
+
+# i = 2
+
+# while i <= 10:
+#     print(i)
+#     i += 2
+
+#=========================
+# Countdown
+#=========================
+
+# count = 5
+
+# while count > 0:
+#     print(count)
+#     count -= 1
+
+# print("Time's Up!")
+
+#=========================
+# Sum of Numbers
+#=========================
+
+# i = 1
+# total = 0
+
+# while i <= 5:
+#     total += i
+#     i += 1
+
+# print("Sum =", total)
+
+#=========================
+# Multiplication Table
+#=========================
+
+# num = 7
+# i = 1
+
+# while i <= 10:
+#     print(num, "x", i, "=", num * i)
+#     i += 1
+
+#=========================
+# break Statement
+#=========================
+
+# i = 1
+
+# while i <= 10:
+#     if i == 6:
+#         break
+#     print(i)
+#     i += 1
+
+#=========================
+# continue Statement
+#=========================
+
+# i = 0
+
+# while i < 5:
+#     i += 1
+#     if i == 3:
+#         continue
+#     print(i)
+
+#=========================
+#Infinite Loop
+#=========================
+
+# while True:
+#     print("Hello")
