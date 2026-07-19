529dfaed2408442f69df7441662b3946c2074cf4
Kirtan Bhatiya <kirtanBhatiya@example.com>
2026-07-19 11:38:53 +0530

    Added Function.py and Lambda_function.py

---
+++ Function.py
@@ -0,0 +1,119 @@
+#======================
+# Function
+#======================
+
+#======================
+# Syntax
+#======================
+
+# def function_name(parameters):
+    # Code
+#   return value
+
+#======================
+# Simple Function
+#======================
+
+# def greet():
+#     print("Hello, Welcome to Python!")
+
+# greet()
+
+#======================
+# Function with Parameters
+#======================
+
+# def greet(name):
+#     print("Hello", name)
+
+# greet("Kirtan")
+# greet("Rahul")
+
+#======================
+# Function with Return Value
+#======================
+
+# def add(a, b):
+#     return a + b
+
+# result = add(10, 20)
+# print(result)
+
+#======================
+# Find Square
+#======================
+
+# def square(num):
+#     return num * num
+
+# print(square(5))
+
+#======================
+#Check Even or Odd
+#======================
+
+# def even_odd(num):
+#     if num % 2 == 0:
+#         return "Even"
+#     else:
+#         return "Odd"
+
+# print(even_odd(12))
+# print(even_odd(15))
+
+#======================
+# Function Without return
+#======================
+
+# def message():
+#     print("Learning Python Functions")
+
+# result = message()
+# print(result)
+
+#======================
+# Default Parameters
+#======================
+
+# def greet(name="Guest"):
+#     print("Welcome", name)
+
+# greet()
+# greet("Kirtan")
+
+
+#======================
+# Multiple Parameters
+#======================
+
+# def student(name, age, city):
+#     print("Name:", name)
+#     print("Age:", age)
+#     print("City:", city)
+
+# student("Kirtan", 22, "Ahmedabad")
+
+#======================
+# Multiplication Table
+#======================
+
+# def table(num):
+#     for i in range(1, 11):
+#         print(f"{num} x {i} = {num * i}")
+
+# table(7)
+
+
+#======================
+# Function Calling Another Function
+#======================
+
+# def add(a, b):
+#     return a + b
+
+# def display():
+#     result = add(15, 20)
+#     print("Sum =", result)
+
+# display()
+
---
+++ Lambda_function.py
@@ -0,0 +1,49 @@
+#======================
+# Lambda Function
+#======================
+
+#======================
+# Syntax
+#======================
+
+# lambda arguments: expression
+
+#======================
+# Add Two Numbers
+#======================
+
+# add = lambda a, b: a + b
+
+# print(add(10, 20))
+
+#======================
+# Square of a Number
+#======================
+
+# square = lambda x: x * x
+
+# print(square(5))
+
+#======================
+# Check Even or Odd
+#======================
+
+# even_odd = lambda x: "Even" if x % 2 == 0 else "Odd"
+
+# print(even_odd(8))
+# print(even_odd(7))
+
+#======================
+# Sort a List of Tuples
+#======================
+
+# students = [
+#     ("Rahul", 85),
+#     ("Kirtan", 95),
+#     ("Amit", 78)
+# ]
+
+# students.sort(key=lambda x: x[1])
+
+# print(students)
+
